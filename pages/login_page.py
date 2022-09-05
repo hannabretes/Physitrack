741c1d628dc5328f326608085fdb9314957c7072
jaroslawjalocha <47159527+jaroslawjalocha@users.noreply.github.com>
2022-09-05 20:56:43 +0200

    Zmiana fixtur

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,15 +1,13 @@
-from Physitrack.utils.locators import MainPageLocators
-from Physitrack.pages.base_page import BasePage
+import pytest
+from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
-class LoginPage(BasePage):
-    LOGIN = (By.XPATH, '//a[@class="button-secondary button-small margin-left w-button"]')
-    BACK_TO_DEMO_BUTTON = (By.CLASS_NAME, 'link-back-to-demo')
+class LoginPage():
+    login = (By.XPATH, '//a[@class="button-secondary button-small margin-left w-button"]')
+    back_to_demo_button = (By.CLASS_NAME, 'link-back-to-demo')
     def __init__(self, driver):
-        self.locator = MainPageLocators
         super(LoginPage, self).__init__(driver)
-
     def click_login_button(self):
-        self.do_click(self.LOGIN)
+        self.do_click(self.login)
 
     def click_back_to_demo(self):
-        self.do_click(self.BACK_TO_DEMO_BUTTON)
+        self.do_click(self.back_to_demo_button)
