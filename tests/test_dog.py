0cea9d87047c67a29a0705be70d17e70c2e5bc9c
jaroslawjalocha <47159527+jaroslawjalocha@users.noreply.github.com>
2022-09-05 20:54:45 +0200

    Zmiana dzialania fixtury w tescie

--- tests/test_dog.py
+++ tests/test_dog.py
@@ -1,20 +1,14 @@
 import pytest
-from Physitrack.pages.base_page import BasePage
-from Physitrack.pages.login_page import LoginPage
-from Physitrack.pages.select_country_page import SelectCountryPage
-from Physitrack.utils.conftest import BaseTest
-from Physitrack.pages.add_program_page import AddProgramPage
-@pytest.mark.usefixtures("setup")
-class TestDog(BaseTest):
-    def test_page_load(self):
-        print("First testcase")
-        page = BasePage(self.driver)
-        login = LoginPage(self.driver)
-        add_program = AddProgramPage(self.driver)
-        select_country = SelectCountryPage(self.driver)
-        page.open("https://www.physitrack.co.uk/")
-        login.click_login_button()
-        select_country.select_country_USA()
-        login.click_back_to_demo()
-        add_program.add_to_card()
+from pages.select_country_page import SelectCountryPage
+from utils.conftest import driver
+from utils.conftest import login_page
 
+def test_page_load(login_page):
+    print("First testcase")
+    #add_program = AddProgramPage(self.driver)
+    #select_country = SelectCountryPage(self.driver)
+    login_page.open("https://www.physitrack.co.uk/")
+    login_page.click_login_button()
+    #select_country.select_country_USA()
+    login_page.click_back_to_demo()
+    #add_program.add_to_card()
