585895ac6533620a305e2b564df53aa7b2795140
jaroslawjalocha <47159527+jaroslawjalocha@users.noreply.github.com>
2022-09-05 20:57:13 +0200

    Zmiana fixtur

--- pages/select_country_page.py
+++ pages/select_country_page.py
@@ -1,14 +1,12 @@
-from Physitrack.utils.locators import MainPageLocators
-from Physitrack.pages.base_page import BasePage
+from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
-class SelectCountryPage(BasePage):
-    COUNTRY_USA = (By.ID, 'country_us')
-    SELECT_COUNTRY = (By.XPATH, '//button[@name="button" and @class="btn-link pad-top choose-server" ]')
+class SelectCountryPage():
+    country_usa_button = (By.ID, 'country_us')
+    select_country_button = (By.XPATH, '//button[@name="button" and @class="btn-link pad-top choose-server" ]')
     def __init__(self, driver):
         super(SelectCountryPage, self).__init__(driver)
 
-
     def select_country_USA(self):
         self.driver.switch_to.window(self.driver.window_handles[1])
-        self.do_click(self.COUNTRY_USA)
-        self.do_click(self.SELECT_COUNTRY)
+        self.do_click(self.country_usa_button)
+        self.do_click(self.select_country_button)
