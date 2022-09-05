838beb79734e5a567d26035a0e74fadf40350ad9
jaroslawjalocha <47159527+jaroslawjalocha@users.noreply.github.com>
2022-09-05 21:32:27 +0200

    Zmiana pliku

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,11 +1,9 @@
 import pytest
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
-class LoginPage():
+class LoginPage(BasePage):
     login = (By.XPATH, '//a[@class="button-secondary button-small margin-left w-button"]')
     back_to_demo_button = (By.CLASS_NAME, 'link-back-to-demo')
-    def __init__(self, driver):
-        super(LoginPage, self).__init__(driver)
     def click_login_button(self):
         self.do_click(self.login)
 
